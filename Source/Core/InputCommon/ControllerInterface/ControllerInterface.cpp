8ca79feb10ffb35f95a92d168a0a59a962661033
Jordan Woyak <jordan.woyak@gmail.com>
2024-03-11 02:28:18 -0500

    InputCommon: Handle window change in Quartz.

--- Source/Core/InputCommon/ControllerInterface/ControllerInterface.cpp
+++ Source/Core/InputCommon/ControllerInterface/ControllerInterface.cpp
@@ -131,7 +131,6 @@ void ControllerInterface::RefreshDevices(RefreshReason reason)
   // or removing them as we are populating them (causing missing or duplicate devices).
   std::lock_guard lk_population(m_devices_population_mutex);
 
-#if defined(CIFACE_USE_WIN32) && !defined(CIFACE_USE_OSX)
   // If only the window changed, avoid removing and re-adding all devices.
   // Instead only refresh devices that require the window handle.
   if (reason == RefreshReason::WindowChangeOnly)
@@ -143,9 +142,9 @@ void ControllerInterface::RefreshDevices(RefreshReason reason)
 
     if (m_populating_devices_counter.fetch_sub(1) == 1)
       InvokeDevicesChangedCallbacks();
+
     return;
   }
-#endif
 
   m_populating_devices_counter.fetch_add(1);
 
