1e7d92ec9cac9d226526ff31cb3f0d43e57d1f42
JosJuice <josjuice@gmail.com>
2024-06-15 20:02:10 +0200

    Android: Replace log type names map with array
    Storing the log type names in a map results in them getting re-sorted by
    their keys, which doesn't quite give us the sorting we want. In
    particular, the Achievements category ended up being sorted at R (for
    RetroAchivements) instead of at A. Every use of the map is just
    iterating through it, so there's no real reason why it has to be a map
    anyway.

--- Source/Core/Common/Logging/LogManager.h
+++ Source/Core/Common/Logging/LogManager.h
@@ -5,8 +5,8 @@
 
 #include <array>
 #include <cstdarg>
-#include <map>
 #include <string>
+#include <vector>
 
 #include "Common/BitSet.h"
 #include "Common/EnumMap.h"
@@ -35,6 +35,13 @@ public:
 class LogManager
 {
 public:
+  struct LogContainer
+  {
+    const char* m_short_name;
+    const char* m_full_name;
+    bool m_enable = false;
+  };
+
   static LogManager* GetInstance();
   static void Init();
   static void Shutdown();
@@ -49,7 +56,7 @@ public:
   void SetEnable(LogType type, bool enable);
   bool IsEnabled(LogType type, LogLevel level = LogLevel::LNOTICE) const;
 
-  std::map<std::string, std::string> GetLogTypes();
+  std::vector<LogContainer> GetLogTypes();
 
   const char* GetShortName(LogType type) const;
   const char* GetFullName(LogType type) const;
@@ -61,13 +68,6 @@ public:
   void SaveSettings();
 
 private:
-  struct LogContainer
-  {
-    const char* m_short_name;
-    const char* m_full_name;
-    bool m_enable = false;
-  };
-
   LogManager();
   ~LogManager();
 
